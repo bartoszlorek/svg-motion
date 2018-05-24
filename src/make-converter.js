4ba007f8e6ad82e36e3dc978ee22aeacc14419fa
Bartosz Lorek <bery.lorek@gmail.com>
2018-05-24 15:38:49 +0200

    unwrap already rooted svg-motion files

--- src/make-converter.js
+++ src/make-converter.js
@@ -1,9 +1,10 @@
 const convert = require('xml-js')
-const applyStyle = require('./.internal/apply-style')
 const omit = require('./.utils/omit')
+const applyStyle = require('./.internal/apply-style')
 
-const INCLUDE_ELEMENTS = ['path', 'g']
+const INCLUDE_ELEMS = ['path', 'g']
 const EXCLUDE_ATTRS = ['id', 'display', 'visibility']
+const SVG_CLASS = 'svg-motion'
 
 module.exports = function(config) {
     return data => {
@@ -13,9 +14,15 @@ module.exports = function(config) {
 
         const svg = svgjs.elements[0]
 
+        // unwrap rooted frames
+        if (svg.attributes.class === SVG_CLASS) {
+            svg.elements = svg.elements[1].elements
+        }
+
         // recreate svg attributes
         svg.attributes = {
             width: '100%',
+            class: SVG_CLASS,
             viewBox: svg.attributes.viewBox,
             preserveAspectRatio: 'xMidYMid meet',
             xmlns: 'http://www.w3.org/2000/svg',
@@ -23,14 +30,13 @@ module.exports = function(config) {
             'xml:space': 'preserve'
         }
 
-        // wrap root element around frames
+        // wrap root around frames
         svg.elements = [{
             type: 'element',
             name: 'g',
-            attributes: {},
             elements: svg.elements
                 .filter(node => {
-                    return INCLUDE_ELEMENTS.indexOf(node.name) !== -1
+                    return INCLUDE_ELEMS.indexOf(node.name) !== -1
                 })
                 .map((frame, index) => {
                     frame.attributes = omit(frame.attributes, EXCLUDE_ATTRS)
